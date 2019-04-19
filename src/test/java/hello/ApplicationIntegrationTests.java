019b2b39a8f1ccae6c7e2fd07261a7c8750f6742
Andersson Garcia Sotelo <anderssongarciasotelo@gmail.com>
2019-04-19 15:05:02 -0500

    Formatting tests

--- src/test/java/hello/ApplicationIntegrationTests.java
+++ src/test/java/hello/ApplicationIntegrationTests.java
@@ -34,24 +34,23 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationIntegrationTests {
 
-    private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 
-    @LocalServerPort
-    private int port = 0;
+	@LocalServerPort
+	private int port = 0;
 
-    @Before
-    public void init() throws Exception {
-        marshaller.setPackagesToScan(ClassUtils.getPackageName(GetCountryRequest.class));
-        marshaller.afterPropertiesSet();
-    }
+	@Before
+	public void init() throws Exception {
+		marshaller.setPackagesToScan(ClassUtils.getPackageName(GetCountryRequest.class));
+		marshaller.afterPropertiesSet();
+	}
 
-    @Test
-    public void testSendAndReceive() {
-        WebServiceTemplate ws = new WebServiceTemplate(marshaller);
-        GetCountryRequest request = new GetCountryRequest();
-        request.setName("Spain");
+	@Test
+	public void testSendAndReceive() {
+		WebServiceTemplate ws = new WebServiceTemplate(marshaller);
+		GetCountryRequest request = new GetCountryRequest();
+		request.setName("Spain");
 
-        assertThat(ws.marshalSendAndReceive("http://localhost:"
-                + port + "/ws", request)).isNotNull();
-    }
+		assertThat(ws.marshalSendAndReceive("http://localhost:" + port + "/ws", request)).isNotNull();
+	}
 }
